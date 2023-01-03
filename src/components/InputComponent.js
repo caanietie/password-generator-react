ab7c05633c3848fc05ba31f007bd6cbc101e4ef1
Anietie <aakjhonclems@gmail.com>
2023-01-03 05:38:35 +0100

    Added feature that controls generated password visibility

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import copy from "../assets/copy.png";
+import eyeAllowed from "../assets/eye-allowed.png";
+import eyeBlocked from "../assets/eye-blocked.png";
 
 export default function InputComponent(props) {
+  const [showPassword, setShowPassword] = useState(false);
+  const { generatedPassword, copyGeneratedPassword } = props;
+  const [alt, src, val] = showPassword ?
+    ["show password", eyeAllowed, "＊".repeat(generatedPassword.length)] :
+    ["hide password", eyeBlocked, generatedPassword];
   return (
     <div className="inputComponent" data-testid="inputComponent">
-      <input type="text" readOnly={true} value={props.generatedPassword} />
-      <button onClick={() => props.copyGeneratedPassword()}>
-        <img alt="copy" src={copy} width={25} height={25} />
-      </button>
+      <input type="text" readOnly={true} value={val} />
+      <div>
+        <button onClick={() => copyGeneratedPassword()}>
+          <img alt="copy" src={copy} width={25} height={25} title="Copy" />
+        </button>
+        <button onClick={() => setShowPassword(!showPassword)}>
+          <img alt={alt} src={src} width={25} height={25} title={alt} />
+        </button>
+      </div>
     </div>
   )
 }
